f8d7da3edee308b465fa6e68482f666782e45c85
Izabela Kuc <isabela.kuc@gmail.com>
2025-11-18 21:26:30 -0800

    toggleHideDone interaction, deleted import React from "React"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+import { useState } from "react";
 
 const tasks = [
   { id: 1, content: "przejsc na Reacta", done: false },
   { id: 2, content: "zjesc obiad", done: true },
 ];
 
-const hideDone = false;
-
 function App() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone((hideDone) => !hideDone);
+  };
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -21,7 +24,13 @@ function App() {
       <Section
         title="Lista zadań"
         body={<Tasks tasks={tasks} hideDone={hideDone} />}
-        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} />}
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+          />
+        }
       />
     </Container>
   );
