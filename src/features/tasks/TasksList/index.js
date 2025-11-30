88bd8afd412a0a529199c5cedbffe38ac74a792c
Izabela Kuc <isabela.kuc@gmail.com>
2025-11-29 20:37:54 -0800

    correct code to TaskList

--- src/features/tasks/TasksList/index.js
+++ src/features/tasks/TasksList/index.js
@@ -1,6 +1,6 @@
 import { List, Item, Content, Button } from "./styled";
 
-const TasksList = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
+const TaskList = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
   <List>
     {tasks.map((task) => (
       <Item key={task.id} hidden={task.done && hideDone}>
