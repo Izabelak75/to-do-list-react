bfee4a356e50e0392a91155f14bfbf7cf6278ed5
Izabela Kuc <isabela.kuc@gmail.com>
2025-11-30 01:34:52 -0800

    delete useTasks, addNewtask, add useSelector

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,47 +1,20 @@
+import { useSelector } from "react-redux";
 import Form from "./Form";
 import TaskList from "./TaskList";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import Container from "../../common/Container";
-import { useTasks } from "../../useTasks";
 
 function Tasks() {
-  const {
-    tasks,
-    removeTask,
-    toggleTaskDone,
-    setAllDone,
-    addNewTask,
-    hideDone,
-    toggleHideDone,
-  } = useTasks();
-
   return (
     <Container>
       <Header title="Lista zadań" />
-      <Section
-        title="Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
-      />
+      <Section title="Dodaj nowe zadanie" body={<Form />} />
       <Section
         title="Lista zadań"
-        body={
-          <TaskList
-            tasks={tasks}
-            hideDone={hideDone}
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone}
-          />
-        }
-        extraHeaderContent={
-          <Buttons
-            tasks={tasks}
-            hideDone={hideDone}
-            toggleHideDone={toggleHideDone}
-            setAllDone={setAllDone}
-          />
-        }
+        body={<TaskList />}
+        extraHeaderContent={<Buttons />}
       />
     </Container>
   );
