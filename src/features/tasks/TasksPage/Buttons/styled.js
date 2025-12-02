812787b5c6b1ac9bc1c2d83bd5ea9d4f63ae0a08
Izabela Kuc <isabela.kuc@gmail.com>
2025-12-01 21:50:23 -0800

    update formating Buttons styled.js

--- src/features/tasks/TasksPage/Buttons/styled.js
+++ src/features/tasks/TasksPage/Buttons/styled.js
@@ -11,6 +11,7 @@ export const Button = styled.button`
   border: none;
   margin: 0 0 0 20px;
   transition: filter 0.3s;
+
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
     flex-basis: 100%;
     margin: 10px;
